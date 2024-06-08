dcd3363dc837500046d2d39cb309bf60446aa886
emcalli2 <emcalli2@arizona.edu>
2024-06-07 17:59:17 -0700

    created second largest and call imported

---
+++ short_probs/proj01-short/call_imported_funcs.py
@@ -0,0 +1,10 @@
+from short1_thing import *
+line = input()
+foo_line = foo(line)
+print(foo_line)
+line2 = input()
+line3 = input()
+bar_line = bar(line,line2,line3)
+print(bar_line)
+baz_line = baz(foo_line,bar_line)
+print(baz_line)
