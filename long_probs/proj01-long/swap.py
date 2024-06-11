9892e6ccd1245f08192de23503ea0d60b2da04a5
emcalli2 <emcalli2@arizona.edu>
2024-06-11 16:07:18 -0700

    completed and documented

--- long_probs/proj01-long/swap.py
+++ long_probs/proj01-long/swap.py
@@ -1,11 +1,23 @@
+""" File: swap.py
+    Author: Elizabeth McAllister
+    Purpose: Sorts words and count gathered from an inputted file
+    Class: Csc 120 Summer 2024
+"""
 def swap():
+    """This function swaps the letters of a string at the halfway point
+       Arguments:none
+       Return Value: the swapped string
+       Pre-condition: there is one string per line
+    """
     line = input("Please give a string to swap:")
     line = line.strip()
     swapped = ""
     half = int(len(line)//2)
+    # checks if the length is even
     if len(line) % 2 == 0:
         swapped = line[half:] + line[:half]
     else:
+        # if the length is not even, the middle char stays the same
         swapped = line[half + 1:] + line[half] + line[:half]
     return swapped
 
