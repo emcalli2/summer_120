0c40750a27c801ff6172434f1c00352bcd065201
emcalli2 <emcalli2@arizona.edu>
2024-06-11 16:04:41 -0700

    completed and documented

--- long_probs/proj01-long/population.py
+++ long_probs/proj01-long/population.py
@@ -1,4 +1,19 @@
+""" File: population.py
+    Author: Elizabeth McAllister
+    Purpose: gains and presents information about
+            the population of various states
+    Class: Csc 120 Summer 2024
+"""
 def population():
+    """This function prints the state and population 
+        of each line in an inputted file, as well as
+        gives a total state number and population
+        count for the whole file
+       Arguments:none
+       Return Value: none
+       Pre-condition: none
+
+    """
     filename = input("file: ")
     filename = filename.strip()
     infile = open(filename, "r")
@@ -8,9 +23,11 @@ def population():
     for line in infile:
         info = []
         states = ""
+        # ignores any lines that are empty or contain #
         if line[0] != "#" and len(line) > 1:
             line = line.strip().split()
             for word in line:
+                # checks if the word is a letter
                 if word.isalpha():
                     states = states + " " + word
                 states = states.strip()
@@ -23,7 +40,7 @@ def population():
             print("")
     print("# of States/Territories: " + str(num_of_terr))
     print("Total Population: " + str(population))
-
+    print("CLOSE FILE")
 def main():
     population()
 main()
