8a4a850b2df3baf6c5fd6c5d1d5b1a29ccd2e892
emcalli2 <142628454+emcalli2@users.noreply.github.com>
2024-06-19 12:04:41 -0700

    documented and completed

--- long_probs/proj02-long/twine.py
+++ long_probs/proj02-long/twine.py
@@ -1,5 +1,22 @@
-
+""" 
+    File: twine.py
+    Author: Elizabeth McAllister
+    Purpose: this program simulates walking in a forrest lost
+            the user inputs directions to move and while
+            they move, a string of twine is placed so that 
+            steps can be retraced. The user can move n,w,s,e, and
+            backwards. As well as can call functions such as range and
+            crossings(the amount of times they have been in that spot before)
+    Class: Csc 120 Summer 2024
+"""
 def commands(twine,move):
+    """ 
+        This function implements various commands such a n,w,s,e, and back
+        as well as handles errors and empty lines.
+        Arguments: twine (the list of tuples of coordinates)
+        Return Value: twine
+        Pre-condition: none
+    """
     curr_pos = twine[len(twine)-1]
     if len(move.split()) > 1:
         print("ERROR: more than one word was given")
@@ -14,21 +31,25 @@ def commands(twine,move):
         twine.pop()
         print("You retrace your steps by one space")
         return twine
+    # moves north
     if move in "n":
         lst = list(curr_pos)
         lst[1] += 1
         twine.append(tuple(lst))
         return twine
+    # moves south
     if move in "s":
         lst = list(curr_pos)
         lst[1] -= 1
         twine.append(tuple(lst))
         return twine
+    # moves east
     if move in "e":
         lst = list(curr_pos)
         lst[0] += 1
         twine.append(tuple(lst))
         return twine
+    # moves west
     if move in "w":
         lst = list(curr_pos)
         lst[0] -= 1
@@ -39,24 +60,41 @@ def commands(twine,move):
         return twine
 
 def path_map(twine,move):
+    """ 
+        This is an unfinished version of path_map which places "." where the 
+        user has traveled based on the twine list, and a X where the user currently is
+        Arguments: twine(list of tuple coordinates)
+        Return Value: none
+        Pre-condition: map has to be typed into the input for this function to be called
+    """
     top_bottom = "+---------+"
     middle = []
     print(top_bottom)
     for i in range(5):
         middle.append(["|         |"])
+    # adds a "*" to the coordinate (0,0)
     middle.append(["|    *    |"])
     for i in range(5):
         middle.append(["|         |"])
+    # adds dots where the user has been
     for i in range(len(twine)-1):
         lst = list(twine[i+1])
         x = lst[0]
         y = lst[1]
         middle[5-y][0] = "|" + (" " * (x+4)) + "." + (" " * (x+4))+ "|"
+    # prints the whole middle of the map
     for i in range(len(middle)):
         print(middle[i][0])
     print(top_bottom)
 
 def crossing(twine):
+    """ 
+        This function determines how many times the user has previously
+        been at their current location 
+        Arguments: twine(list of tuple corrdinates)
+        Return Value: none
+        Pre-condition: user must type crossings to call function
+    """
     curr_pos = twine[len(twine)-1]
     count = 0
     for i in range(len(twine)):
@@ -65,6 +103,14 @@ def crossing(twine):
     print("There have been " + str(count) + " times in the history when you were at this point.")
 
 def ranges(twine):
+    """ 
+        This function determines the range of the path the user has taken
+        Arguments: twine(list of tuple corrdinates)
+        Return Value: none
+        Pre-condition: user must input "range" to call this function
+        Note: east is highest x value, west is the lowest x value, south
+            is the lowest y value, north is the highest y value
+    """
     east = None
     west = None
     south = None
@@ -109,4 +155,3 @@ def main():
         print()
 
 main()
-
