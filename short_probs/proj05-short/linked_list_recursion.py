2b9509c4c87980045edf1ccd0ffdde4d07d58e6d
emcalli2 <142628454+emcalli2@users.noreply.github.com>
2024-06-25 13:24:54 -0700

    completed

--- short_probs/proj05-short/linked_list_recursion.py
+++ short_probs/proj05-short/linked_list_recursion.py
@@ -1,13 +1,24 @@
 def is_plus_two_recursive(head):
-    if head is None:
-        print("hello")
+    if head.next is None:
         return True
     else:
         if head.next is not None and head.next.val == head.val + 2:
-            print(head.next.val)
-            print(head.val + 2)
             is_plus_two_recursive(head.next)
-        return False
+            return True
+        else:
+            return False
+
+def is_plus_two(head):
+    if head is None:
+        return True
+    if head.next is None:
+        return True
+    curr = head
+    while curr is not None:
+        if curr.next is not None and curr.next.val != curr.val + 2:
+            return False
+        curr = curr.next
+    return True
     
         
 
