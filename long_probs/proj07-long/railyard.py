20ccf1c02ee513579446cd15827818de79c158e1
emcalli2 <142628454+emcalli2@users.noreply.github.com>
2024-07-24 13:07:15 -0700

    finished and doccumented (not working code)

--- long_probs/proj07-long/railyard.py
+++ long_probs/proj07-long/railyard.py
@@ -1,19 +1,21 @@
 """ 
     File: railyard.py
     Author: Elizabeth McAllister
-    Purpose: This program deals with a train yard. It reads a yard file and
-        prints out a yard, then takes commands to move the cars and when the 
-        cars are all going to the same destination, the train departs. 
+    Purpose: This program deals with a train yard. It reads a 
+        yard file and prints out a yard, then takes commands to 
+        move the cars and when the cars are all going to the same 
+        destination, the train departs. 
     Class: Csc 120 Summer 2024
 """
 def rail_yard():
     """
-    This function reads an inputted file and creates a rail yard with each
-    line of the file. 
+    This function reads an inputted file and creates a rail 
+    yard with each line of the file. 
     Parameters: None
     Returns: yard - a list of list of strings that represents the yard
     """
-    yardfile = input("Please give yard file:")
+    yardfile = input("Please give the yard file: ")
+    print("")
     infile = open(yardfile, 'r')
     yard = []
     num = 1
@@ -38,14 +40,16 @@ def move_command(command,yard):
     if command[1] == 0:
         print_yard(yard)
         return
-    from_track = int(command[2])
-    to_track = int(command[3])
+    from_track = int(command[2]) -1
+    to_track = int(command[3]) -1
     length = len(yard[from_track][0]) - number
     cars = yard[from_track][0][-number:-1]
     yard[to_track][0] = yard[to_track][0][number:] + cars + "-"
     # ensures that the length is the same after the cars are removed
     yard[from_track][0] = ("-" * (number-1)) + yard[from_track][0][:length] + "-"
+    print("The locomotive on track "+str(from_track+1)+" moved "+str(number -1)+" cars to track "+str(to_track+1)+".")
     print_yard(yard)
+
 def debug_dump(yard):
     """
     This function "dumps" information about the current state of
@@ -58,7 +62,7 @@ def debug_dump(yard):
         content = []
         print("Track #" + str(i))
         print("  Length: " + str(len(yard[i][0])-2))
-        for k in range(len(yard[i][0])-1):
+        for k in range(len(yard[i][0])):
             if yard[i][0][-k] != "-":
                 content.append(yard[i][0][-k])
         print("  Contents: " + str(content))
@@ -66,69 +70,52 @@ def debug_dump(yard):
 
 def departing_train(yard):
     """
-    This function departs trains that have all cars going to the same location.
+    This function departs trains that have all cars going to the 
+    same location.
     Parameters: yard - a list of list of strings that make up the yard
     Returns: None
     """
-    departing = None
     for i in range(len(yard)):
+        departing = False
         cars = 0
-        for j in range(len(yard[i][0])-3):
-            if yard[i][0][j] not in "-":
-                if yard[i][0][j] == yard[i][0][j+1]:
-                    cars += 1
-                    departing = True
-                else: 
-                    # if one of the cars is not the same destination, the loop breaks
-                    departing = False
-                    break
+        if "T" in yard[i][0]:
+            for j in range(len(yard[i][0])-3):
+                if "-" not in yard[i][0][j]:
+                    if yard[i][0][j] == yard[i][0][j+1]:
+                        cars += 1
+                        departing = True
+                    else: 
+                        departing = False
+                        break
         if departing == True:
-            print("*** ALERT***  The train on track " + str(i + 1) + ", which had " + str(cars + 1) +
-            "cars, departs for destination " + str(yard[i][0][j]) +  ".")
+            print("*** ALERT***  The train on track " + str(i + 1) + ", which had " + str(cars+1) +
+            " cars, departs for destination " + str(yard[i][0][j]) +  ".")
             yard[i][0] = "-" * len(yard[i][0])
-    # calls the print_yard function to print the new yard
     print_yard(yard)
-        
-
+       
 def print_yard(yard):
     """
-    This function prints out the rail yard as well as information about it
-    such as destination number and locomotive numbers.
+    This function prints out the rail yard as well as information 
+    about it such as destination number and locomotive numbers.
     Parameters: yard - a list of list of strings that make up the yard
     Returns: None
     """
     num = 1
     loco = 0
     destination = 0
+    destination_list = ["T"]
     for i in range(len(yard)):
         print(str(num) + ": " + yard[i][0])
         num += 1
         for j in range(len(yard[i][0])):
             if "T" in yard[i][0][j]:
                 loco += 1
-            elif "a" in yard[i][0][j]:
-                if destination == 2:
-                    destination = 3
-                if destination == 1:
-                    destination = 2
-                if destination == 0:
-                    destination = 1
-                
-            elif "b" in yard[i][0][j]:
-                if destination == 2:
-                    destination = 3
-                if destination == 1:
-                    destination = 2
-                if destination == 0:
-                    destination = 1
-
-            elif "c" in yard[i][0][j]:
-                if destination == 2:
-                    destination = 3
-                if destination == 1:
-                    destination = 2
-                if destination == 0:
-                    destination = 1
+            elif yard[i][0][j] != "-" and yard[i][0][j] not in destination_list:
+                destination += 1
+                destination_list.append(yard[i][0][j])  
+    if loco == 0:
+        print("Quitting!")
+        quit
     print("Locomotive count: " + str(loco))
     print("Destination count: " + str(destination))
     
@@ -141,11 +128,16 @@ def main():
     Returns: None
     """
     yard = rail_yard()
-    command = input("What is your next command?")
+    print("")
+    command = input("What is your next command? ")
+    print("")
     while "quit" not in command:
         if command == "dump":
+            print("")
             debug_dump(yard)
-            command = input("What is your next command?")
+            print("")
+            command = input("What is your next command? ")
+            print("")
         if "move" in command:
             command = command.strip().split()
             if len(command) != 4:
@@ -163,7 +155,11 @@ def main():
             else:
                 move_command(command,yard)
                 departing_train(yard)
-            command = input("What is your next command?")
+            print("")
+            print_yard(yard)
+            print("")
+            command = input("What is your next command? ")
+            print("")
     if "quit" in command:
         print("Quitting!")
 main()
